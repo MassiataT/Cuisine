81e90cc849f843a497793b8d7510b81369a56462
Massiata Touré <massiata_toure@hotmail.com>
2023-03-23 13:38:35 +0100

    correction de quelques bugs d'affichage

--- pages/recettes/v_recettes.php
+++ pages/recettes/v_recettes.php
@@ -1,32 +1,34 @@
 <h1>Recettes</h1>
 <br><br>
-<?php
+<div class="page">
+    <?php
 
-foreach ($recipes as $value) {
-    echo '<a class="lien" href="../recette/c_recette.php?id=' . $value['id'] . '">';
-    echo '<div id="part">';
+    foreach ($recipes as $value) {
+        echo '<div id="part">';
+        echo '<a class="lien" href="../recette/c_recette.php?id=' . $value['id'] . '">';
 
-    $image = $recette->image($value['id']);
-    $image = $image->fetch();
-    echo '<img class="image-fluid rounded" src="data:image/jpeg;base64,' . base64_encode($image['bin']) . '"/>';
+        $image = $recette->image($value['id']);
+        $image = $image->fetch();
+        echo '<img class="image-fluid rounded" src="data:image/jpeg;base64,' . base64_encode($image['bin']) . '"/>';
 
-    echo '<div id="text">';
+        echo '<div id="text">';
 
-    echo "<h2>" . $value['name'] . "</h2>";
+        echo "<h2>" . $value['name'] . "</h2>";
 
-    echo "<p>" . $value['description'] . "</p>";
-    echo '</a>';
-    foreach ($user as $u) {
-        if ($u['id'] === $value['id']) {
-            echo "<a href=\"../compte/c_compte.php?nickname=" . $u['nickname'] . "\">De " . $u['nickname'] . "</a>";
+        echo "<p>" . $value['description'] . "</p>";
+        echo '</a>';
+        foreach ($user as $u) {
+            if ($u['id'] === $value['id']) {
+                echo "<a href=\"../compte/c_compte.php?nickname=" . $u['nickname'] . "\">De " . $u['nickname'] . "</a>";
+            }
         }
+
+        echo '</div>';
+
+        echo '</div>';
+
+        echo "<br><br>";
     }
 
-    echo '</div>';
-
-    echo '</div>';
-
-    echo "<br><br>";
-}
-
-?>
+    ?>
+</div>
